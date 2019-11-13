6f01b7b5f4fcfe396cbae817db549f7f47e9faf8
aseemagarwal123 <35007308+aseemagarwal123@users.noreply.github.com>
2019-11-14 02:47:47 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 // db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://aseem:aseem123@wtf-m8db4.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true })
+mongoose.connect('mongodb+srv://aseem:'+process.env.password+'@wtf-m8db4.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true })
 .then((d)=>{console.log('database connected')})
 .catch((err)=>{console.log('database error'+err)})
